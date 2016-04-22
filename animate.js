fed2b008a35fc8a44c3fcbdfbbc1879f2f4b6abd
lihelennn <helen82198@gmail.com>
2016-04-22 06:08:38 -0400

    IT WORKSS

--- animate.js
+++ animate.js
@@ -4,36 +4,43 @@ var s = document.getElementById("stop");
 var dvd = document.getElementById("dvd");
 var width = c.width;
 var height = c.height;
-var dvdX = width/2;
-var dvdY = height/2;
-var movingRight = true;
-var movingUp = true;
-var logo = new Image();
+var arrayBunnies = [];
 
+var spawn = function(){
+    console.log("hello");
+    var f1 = makeDVD("bunnies1.png");
+    f1.bounce();
+    arrayBunnies.push(f1);
+};
 
-var makePlayground = function(imgName){
-    logo.src = imgName;
-
-
-
-    var drawOutside = function(){
-	ctx.rect(0,0,width,height);
-	ctx.stroke();
-    }
+var clear = function(e){
+    ctx.clearRect(0,0,width,height);
     drawOutside();
+}
+
+var drawOutside = function(){
+    ctx.rect(0,0,width,height);
+    ctx.stroke();
+}
+drawOutside();
+
+var stop = function(e){
+    window.cancelAnimationFrame(hello);
+}
 
 
-    var clear = function(e){
-	ctx.clearRect(0,0,width,height);
-	drawOutside();
-    }
-
-    var loadDVD = function(e){
-	clear(); 
-	ctx.drawImage(logo,dvdX,dvdY,100,100);
-    }
+var makeDVD = function(imgName){
+    var dvdX = Math.random()*width/2;
+    var dvdY = Math.random()*height/2;
+    var movingRight = true;
+    var movingUp = true;
+    var logo = new Image();
+    logo.src = imgName;
+    //loadDVD
+    //ctx.drawImage(logo,dvdX,dvdY,100,100);
     
-    var bounce = function(e){
+    var bounce = function(){
+
 	if (movingUp){
 	    dvdY--;
 	}else{
@@ -60,27 +67,59 @@ var makePlayground = function(imgName){
 		movingUp = false;
 	    }
 	}
-	clear();
-	drawOutside();
-	ctx.drawImage(logo,dvdX,dvdY,100,100);
-	hello = window.requestAnimationFrame(bounce());
+	
+	//hello = window.requestAnimationFrame(bounce);
 
     }
     
-    hello = window.requestAnimationFrame(bounce());
-
-    var stop = function(e){
-	window.cancelAnimationFrame(hello);
-    }
-
-    return {
-	clear: clear,
-	loadDVD: loadDVD,
+    return {	
+	logo: logo,
+	movingRight: movingRight,
+	movingUp: movingUp,
+	dvdX: dvdX,
+	dvdY: dvdY,
 	bounce: bounce,
-	stop: stop,
     }
 };
 
-var f1 = makePlayground("bunnies1.png");
-s.addEventListener("click",f1.stop());
-dvd.addEventListener("click",f1.bounce());
+
+s.addEventListener("click",stop);
+
+var animate = function(){
+    clear();
+    for (i=0; i<arrayBunnies.length;i++){
+	currentBunny = arrayBunnies[i];
+	if (currentBunny.movingUp){
+	    currentBunny.dvdY--;
+	}else{
+	    currentBunny.dvdY++;
+	}
+
+	if (currentBunny.movingRight){
+	    currentBunny.dvdX++;
+	}else{
+	    currentBunny.dvdX--;
+	}
+
+	if (currentBunny.dvdX >= width-100){
+	    currentBunny.movingRight = false;
+	}else{
+	    if (currentBunny.dvdX <= 0){
+		currentBunny.movingRight = true;
+	    }
+	}
+	if (currentBunny.dvdY >= height-100){
+	    currentBunny.movingUp = true;
+	}else{
+	    if (currentBunny.dvdY <= 0){
+		currentBunny.movingUp = false;
+	    }
+	}
+	console.log(currentBunny.logo);
+	ctx.drawImage(currentBunny.logo,currentBunny.dvdX,currentBunny.dvdY,100,100);
+    }
+    hello = window.requestAnimationFrame(animate);
+
+}
+
+animate();
